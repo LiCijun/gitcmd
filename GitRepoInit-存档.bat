4dfebb7ac9647e8c4ad31b840f709515671dbd3e
li <lcj211@aihlp.com>
2018-11-07 00:19:00 +0800

    2

--- GitRepoInit-存档.bat
+++ GitRepoInit-存档.bat
@@ -8,7 +8,15 @@ mkdir OA
 cd OA
 git init
 git remote add li  li:li/OA.git
-git remote add KMT  kmt:OA.git
 git remote add lcj  lcj:li/OA.git
+
+
+
+cd %~dp0..\
+mkdir photo
+cd photo
+git init
+git remote add li  li:li/photo.git
+git remote add lcj  lcj:li/photo.git
 
 
