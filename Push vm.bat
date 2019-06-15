aa69feb93ff3f05a51aebc729d289cb48f6b17c6
li <lcj211@aihlp.com>
2019-06-15 18:14:12 +0800

    OK

--- Push vm.bat
+++ Push vm.bat
@@ -1,22 +1,22 @@
 @echo off
  for /d %%s in (%~dp0..\*) do (
-echo Push CMD
+echo Push vm
 cd %%s
 echo %%s
 echo.
 git.exe push --all  --progress  "vm"
-git.exe push --tags --progress  "vm" master:master
+git.exe push --tags --progress  "vm"
 echo.
 echo.
 echo.
 )
  for /d %%s in (%~dp0..\AndroidLib\*) do (
-echo Push CMD
+echo Push vm
 cd %%s
 echo %%s
 echo.
 git.exe push --all  --progress  "vm"
-git.exe push --tags --progress  "vm" master:master
+git.exe push --tags --progress  "vm"
 echo.
 echo.
 echo.
