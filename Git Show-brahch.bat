06e35d6e54d10f993b4feb3977cb7d771b185f6f
lcj211 <lcj211@aihlp.com>
2019-11-05 20:04:44 +0800

    gitcmd

--- Git Show-brahch.bat
+++
@@ -1,9 +0,0 @@
-@echo off
- for /d %%s in (%~dp0..\*) do (
-cd %%s
-echo %%s
-git.exe show-branch
-echo.
-echo.
-)
-pause
---
+++ fsck.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+
+function getdir(){
+
+
+    for file in $work_path/*
+    do  
+    if test -d $file
+    then    
+        
+        if [   -d ${file}'/.git'   ] ; then 
+          
+     cd $file
+      echo $file ;  
+    git  fsck
+        fi   
+    fi
+    done
+    
+}
+
+work_path=$(dirname $(dirname $(readlink -f $0)))
+getdir 
+work_path=$work_path/AndroidLib
+getdir 
+
+
+
+
+
