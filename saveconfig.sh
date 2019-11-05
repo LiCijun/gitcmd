56c56f6f144ab09e06b7ac45f766a635b6536f45
lcj211 <lcj211@aihlp.com>
2019-11-05 11:11:58 +0800

    saveconfig

---
+++ saveconfig.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+cd /home/li/.IntelliJIdea2019.2
+pwd
+git add . && git commit -m "auto commit" && git push -v --progress --all  vm
+echo 
+cd /home/li/.CLion2019.2
+pwd
+git add . && git commit -m "auto commit" && git push -v --progress --all  vm
+echo 
+cd  /home/li/.AndroidStudio3.5
+pwd
+git add . && git commit -m "auto commit" && git push -v --progress --all  vm
