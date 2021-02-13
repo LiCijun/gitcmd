bbc14bc395191386d6a75bb9ed7cafe835be05a4
li <lcj211@aihlp.com>
2021-02-13 21:18:36 +0800

    修改：     saveconfig.sh

--- saveconfig.sh
+++ saveconfig.sh
@@ -2,24 +2,41 @@
 
 cd /home/li/.config/JetBrains/IntelliJIdea2020.3
 pwd
+git init
 git add --all  && git commit -m "auto commit" 
 echo 
 
 cd /home/li/.local/share/JetBrains/IntelliJIdea2020.3
 pwd
-git add --all  && git commit -m "auto commit" 
-echo 
-
-cd /home/li/.config/JetBrains/CLion2020.1
-pwd
+git init
 git add --all  && git commit -m "auto commit" 
 echo 
 
 
-cd  /home/li/.AndroidStudio4.0
+
+cd  /home/li/.config/Google/AndroidStudio4.1/
 pwd
+git init
 git add --all  && git commit -m "auto commit" 
+echo 
 
 
+cd  /home/li/.local/share/Google/AndroidStudio4.1/
+pwd
+git init
+git add --all  && git commit -m "auto commit" 
+echo 
 
 
+cd  /home/li/.config/JetBrains/CLion2020.3
+pwd
+git init
+git add --all  && git commit -m "auto commit" 
+echo 
+
+
+cd  /home/li/.local/share/JetBrains/CLion2020.3
+pwd
+git init
+git add --all  && git commit -m "auto commit" 
+echo 
